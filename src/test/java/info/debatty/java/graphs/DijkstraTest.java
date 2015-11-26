f8cf76725ba13ccf0756332783eb2c2f4fede32f
Thibault Debatty <thibault.debatty@gmail.com>
2015-11-26 14:26:34 +0100

    Added test for Dijkstra algorithm

---
+++ src/test/java/info/debatty/java/graphs/DijkstraTest.java
@@ -0,0 +1,120 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Thibault Debatty.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package info.debatty.java.graphs;
+
+import info.debatty.java.graphs.build.Brute;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Thibault Debatty
+ */
+public class DijkstraTest extends TestCase {
+    
+    public DijkstraTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getPath method, of class Dijkstra.
+     * @throws java.lang.Exception
+     */
+    public void testGetPath() throws Exception {
+
+        // Generate some nodes
+        int count = 1000;
+        ArrayList<Node> nodes = new ArrayList<Node>(count);
+        for (int i = 0; i < count; i++) {
+            // The value of our nodes will be an int
+            nodes.add(new Node<Integer>(String.valueOf(i), i));
+        }
+        
+        // Instantiate and configure the brute-force graph building algorithm
+        // The minimum is to define k (number of edges per node)
+        // and a similarity metric between nodes
+        Brute builder = new Brute<Integer>();
+        builder.setK(10);
+        builder.setSimilarity(new SimilarityInterface<Integer>() {
+
+            public double similarity(Integer value1, Integer value2) {
+                return 1.0 / (1.0 + Math.abs(value1 - value2));
+            }
+        });
+        
+        // Run the algorithm, and get the resulting neighbor lists
+        Graph<Integer> graph = builder.computeGraph(nodes);
+        
+        // Compute shortest paths from node0 to all other nodes
+        Dijkstra dijkstra = new Dijkstra(graph, nodes.get(0));
+        assertEquals(200, dijkstra.getPath(nodes.get(999)).size());
+        
+    }
+
+    /**
+     * Test of getLargestDistance method, of class Dijkstra.
+     */
+    public void testGetLargestDistance() {
+        // Generate some nodes
+        int count = 1000;
+        ArrayList<Node> nodes = new ArrayList<Node>(count);
+        for (int i = 0; i < count; i++) {
+            // The value of our nodes will be an int
+            nodes.add(new Node<Integer>(String.valueOf(i), i));
+        }
+        
+        // Instantiate and configure the brute-force graph building algorithm
+        // The minimum is to define k (number of edges per node)
+        // and a similarity metric between nodes
+        Brute builder = new Brute<Integer>();
+        builder.setK(10);
+        builder.setSimilarity(new SimilarityInterface<Integer>() {
+
+            public double similarity(Integer value1, Integer value2) {
+                return 1.0 / (1.0 + Math.abs(value1 - value2));
+            }
+        });
+        
+        // Run the algorithm, and get the resulting neighbor lists
+        Graph<Integer> graph = builder.computeGraph(nodes);
+        
+        // Compute shortest paths from node0 to all other nodes
+        Dijkstra dijkstra = new Dijkstra(graph, nodes.get(0));
+        
+        assertEquals(199, dijkstra.getLargestDistance());
+    }
+    
+}
